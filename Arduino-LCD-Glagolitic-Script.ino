3d40effa28eab9904fec7c6a389e00eb99da8d7f
Vito Čuić <cuicvito@gmail.com>
2022-05-27 20:51:48 +0200

    gprint now takes a char array as an argument

--- Arduino-LCD-Glagolitic-Script.ino
+++ Arduino-LCD-Glagolitic-Script.ino
@@ -3,9 +3,6 @@
 
 LiquidCrystal_I2C lcd(0x27,20,4);
 
-int count = 0;
-int char_count = 0;
-
 byte mem[][8] = {
         {B00000,B01110,B00100,B00100,B11111,B10101,B10101,B10101},
         {B00000,B10101,B10101,B10111,B11100,B10000,B10000,B11111},
@@ -37,63 +34,63 @@ byte mem[][8] = {
 };
 
 int map_[][4] = {{65,0,0},{66,0,1},{67,0,2},{68,1,3,4},{69,0,5},{70,0,6},{71,0,7},{72,0,8},{73,0,9},{74,1,10,11},{75,0,12},{76,1,13,14},{77,0,15},{78,0,16},{79,0,17},{80,0,18},{82,0,19},{83,0,20},{84,1,21,22},{85,0,23},{86,1,24,25},{90,0,26}};
-void gprint(char x) {
-  int x_int = toupper(x);
-  int index = -1;
 
-  //hardcoded length
-  for (int i = 0; i < 22; i++) {
-    if (x_int == map_[i][0]) {
-        index = i;
+void gprint(char x[]) {
+
+  int x_int;
+  int index;
+
+  int count = 0;
+  int char_count = 0;
+
+  int s = 0;
+
+  for (int k = 0; k < strlen(x); k++) {
+
+    x_int = toupper(x[k]);
+    index = -1;
+
+    //find char in map_ (hardcoded length)
+    for (int i = 0; i < 22; i++) {
+      if (x_int == map_[i][0]) {
+          index = i;
       }
     }
 
+    s = 3;
+    if (map_[index][1] == 1) {
+      s = 4;
+    }
 
-  int s = 3;
-  if (map_[index][1] == 1) {
-    s = 4;
-  }
-  Serial.println(s);
-  for (int i = 2; i < s; i++) {
-      int j = map_[index][i];
-      //Serial.println(j);
-      lcd.createChar(count, mem[j]);
-      count++;
-  }
+    //store char in lcd memory (max 8 chars)
+    for (int i = 2; i < s; i++) {
+        int j = map_[index][i];
+        lcd.createChar(count, mem[j]);
+        count++;
+    }
 
-  char_count += s-2;
+    char_count += s-2;
+
+  }
 
   lcd.home();
 
+  //print chars
   for (int i = 0; i < char_count; i++) {
       lcd.write(i);
   }  
-
-
   
 }
 
 
 void setup() {
 
-  Serial.begin(9600);
-
   lcd.init();
-
   lcd.backlight();
 
-  char x[] = "VITO";
+  gprint("Vito");  
 
 
-  for (int i = 0; i < strlen(x); i++) {
-    gprint(x[i]);  
-  }
-
-
-
-  
 }
 
-void loop() {
-  
-  }
+void loop() {}
